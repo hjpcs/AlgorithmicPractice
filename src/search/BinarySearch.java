7fca4b60a93cdf5957558d6d9542b062364e242d
hjpcs <hejinpengcs@foxmail.com>
2019-06-11 20:13:31 +0800

    add binarysearch mod

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -45,4 +45,84 @@ public class BinarySearch {
             return binarySearch(a, low, mid - 1, value);
         }
     }
+
+    //查找第一个值等于给定值的元素
+    public int binarySearchFirst(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || a[mid - 1] != value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //查找最后一个值等于给定值的元素
+    public int binarySearchLast(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if (mid == (n - 1) || a[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //查找第一个大于等于给定值的元素
+    public int binarySearchGe(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if (mid == 0 || a[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    //查找最后一个小于等于给定值的元素
+    public int binarySearchLe(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] <= value) {
+                if (mid == (n - 1) || a[mid + 1] > value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
